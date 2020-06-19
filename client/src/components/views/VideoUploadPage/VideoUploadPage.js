bd9a22b853c0db77c40d3179d10666e1d3500ee8
HanM.G <hanmoongoo@gmail.com>
2020-06-20 03:07:01 +0900

    multer를 사용해 노드 서버에 파일을 저장 (npm install multer --save)

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {Typography, Button, Form, message, Input, Icon } from 'antd'
 import Dropzone from 'react-dropzone'
+import Axios from 'axios'
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -41,6 +42,26 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value)
     }
 
+    const onDrop = (files) => {
+        let formData = new FormData;
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        
+        formData.append("file", files[0])
+        console.log(files)
+        
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response =>{
+                if(response.data.success){
+                    console.log(response.data)
+                }else{
+                    alert('비디오 업로드에 실패했습니다.')
+                }
+            })
+
+    }
+
 
     return (
         <div style={{maxWidth:'700px', margin:'2rem auto' }}>
@@ -50,11 +71,11 @@ function VideoUploadPage() {
 
             <Form onSubmit>
                 <div style={{display:'flex', justifyContent:'space-between'}}>
-                    {/* Drop Zone */}
+                    {/* Drop Zone */}                                        
                     <Dropzone
-                    onDrop
-                    multiple
-                    maxSize>
+                    onDrop = {onDrop}
+                    multiple = {false}
+                    maxSize = {100000000}>
                         {({getRootProps, getInputProps})=>(
                             <div style={{width:'300px',height:'240px', border:'1px solid lightgray', display:'flex', alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
                                 <input {...getInputProps()}/>
