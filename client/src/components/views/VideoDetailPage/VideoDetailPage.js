773c742e87558fae704dd39f443bb996ba262f4f
HanM.G <hanmoongoo@gmail.com>
2020-06-24 02:06:02 +0900

    비디오 상세페이지에 들어갈 SideVideo 생성

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {Row, Col, List, Avatar} from 'antd'
 import Axios from 'axios'
+import SideVideo from './Sections/SideVideo'
 
 
 function VideoDetailPage(props) {
@@ -40,7 +41,8 @@ function VideoDetailPage(props) {
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
-                    Side Videos
+                    {/* Side Videos */}
+                    <SideVideo />
                 </Col>
             </Row>
         )
