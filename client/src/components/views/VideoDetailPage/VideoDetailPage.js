674d760fe1ab0420739d1b2dcdc821b5540418af
HanM.G <hanmoongoo@gmail.com>
2020-06-28 04:18:09 +0900

    comment컴포넌트, comment 라우팅생성, comment 저장 및 result 확인 까지

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -11,6 +11,7 @@ function VideoDetailPage(props) {
     const videoId = props.match.params.videoId
     const variable = {videoId: videoId}
     const [VideoDetail, setVideoDetail] = useState([])
+    //const [Comments, setComments] = useState([])
     
     useEffect(() => {
         Axios.post('/api/video/getVideoDetail',  variable)
@@ -43,7 +44,7 @@ function VideoDetailPage(props) {
                         </List.Item>
 
                         {/* Conments */}
-                        <Comment />
+                        <Comment postId={videoId} />
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
